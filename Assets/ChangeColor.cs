72d74ec9b818760a2a9b3558546c8a57976a3b34
Manh Vu <vuvanmanhttvtvp@gmail.com>
2021-11-08 16:37:29 +0700

    done

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void Red()
+    {
+        this.GetComponent<Renderer>().material.color = Color.red;
+    }
+    public void Blue()
+    {
+        this.GetComponent<Renderer>().material.color = Color.blue;
+    }
+    public void Black()
+    {
+        this.GetComponent<Renderer>().material.color = Color.black;
+    }
+}
---
+++ Assets/Question.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+    public bool isAnswer = false;
+    static bool isGameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Yellow()
+    {
+        if (!isGameOver)
+            this.GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void White()
+    {
+        if (!isGameOver)
+            this.GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    public void Choose()
+    {
+        if (!isGameOver)
+        {
+            this.GetComponent<Renderer>().material.color = isAnswer ? Color.green : Color.red;
+            isGameOver = true;
+        }
+    }
+
+}
